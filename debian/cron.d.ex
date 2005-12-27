2998a16bdb13e39d078240e733c76f4ba6d2f46f
John Goerzen <jgoerzen@complete.org>
2005-12-27 23:24:27 +0100

    Removed example Debian templates that aren't relevant

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the hdbc package
-#
-0 4	* * *	root	hdbc_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(hdbc):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="hdbc" command="/usr/bin/hdbc"
