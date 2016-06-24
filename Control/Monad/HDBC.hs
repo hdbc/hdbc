5ec4d1a66a1f73a54c2921ea8f3d55d404877e80
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-06-23 19:43:53 -0700

    Changed all the names so I wouldn't have to do an annoying hack

--- Control/Monad/HDBC.hs
+++ Control/Monad/HDBC.hs
@@ -1,25 +1,17 @@
 {-# LANGUAGE RankNTypes, ConstraintKinds, KindSignatures, ConstraintKinds, ScopedTypeVariables #-}
 
-module Control.Monad.HDBC
+module Control.Monad.HDBC.Class
     (
-      IConnection
-    , SqlValue
-    , Statement
-    , SqlColDesc
-    , disconnect, commit, rollback, runRaw
-    , run, prepare, clone, hdbcDriverName 
-    , hdbcClientVer, dbServerVer, dbTransactionSupport
-    , getTables, describeTable, proxiedClientName
+      disconnectW, commitW, rollbackW, runRawW
+    , runW, prepareW, cloneW
+    , getTablesW, describeTableW
+    , module HDBC
     )
 where
 
 import Control.Monad.Reader
-import qualified Database.HDBC as HDBC
-
-type IConnection = HDBC.IConnection
-type SqlValue = HDBC.SqlValue
-type Statement = HDBC.Statement
-type SqlColDesc = HDBC.SqlColDesc
+import Data.Convertible
+import Database.HDBC as HDBC
 
 type WithCon con m a = (IConnection con, MonadIO m) => ReaderT con m a
 
@@ -34,59 +26,34 @@ lift2WC :: (IConnection con) => (con -> x -> IO a) -> x -> WithCon con m a
 
 lift2WC action = lift1WC . flip action
 
-disconnect :: WithCon con m ()
-disconnect = lift1WC HDBC.disconnect
+disconnectW :: WithCon con m ()
+disconnectW = lift1WC HDBC.disconnect
 
-commit :: WithCon con m ()
-commit = lift1WC HDBC.commit
+commitW :: WithCon con m ()
+commitW = lift1WC HDBC.commit
 
-rollback :: WithCon con m ()
-rollback = lift1WC HDBC.rollback
+rollbackW :: WithCon con m ()
+rollbackW = lift1WC HDBC.rollback
 
-runRaw :: String
+runRawW :: String
        -> WithCon con m ()
-runRaw = lift2WC HDBC.runRaw
+runRawW = lift2WC HDBC.runRaw
 
-run :: String
+runW :: String
     -> [SqlValue]
     -> WithCon con m Integer
-run sql = lift1WC . flip (flip HDBC.run sql)
+runW sql = lift1WC . flip (flip HDBC.run sql)
 
-prepare :: String
+prepareW :: String
         -> WithCon con m Statement
-prepare = lift2WC HDBC.prepare
+prepareW = lift2WC HDBC.prepare
 
-clone :: WithCon con m con
-clone = lift1WC HDBC.clone
+cloneW :: WithCon con m con
+cloneW = lift1WC HDBC.clone
 
-hdbcDriverName :: IConnection con 
-               => con
-               -> String
-hdbcDriverName = HDBC.hdbcDriverName
+getTablesW :: WithCon con m [String]
+getTablesW = lift1WC HDBC.getTables
 
-hdbcClientVer :: IConnection con 
-              => con 
-              -> String
-hdbcClientVer = HDBC.hdbcClientVer
-
-proxiedClientName :: IConnection con 
-                  => con 
-                  -> String
-proxiedClientName = HDBC.proxiedClientName
-
-dbServerVer :: IConnection con 
-            => con 
-            -> String
-dbServerVer = HDBC.dbServerVer
-
-dbTransactionSupport :: IConnection con 
-                     => con 
-                     -> Bool
-dbTransactionSupport = HDBC.dbTransactionSupport
-
-getTables :: WithCon con m [String]
-getTables = lift1WC HDBC.getTables
-
-describeTable :: String
+describeTableW :: String
               -> WithCon con m [(String, SqlColDesc)]
-describeTable = lift2WC HDBC.describeTable
+describeTableW = lift2WC HDBC.describeTable
