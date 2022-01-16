9a531674700f0812fd98f951620fbd413a107329
Janus Troelsen <janus@flipstone.com>
2022-01-16 11:03:33 -0600

    Don't require testpack

--- testsrc/TestSqlValue.hs
+++ testsrc/TestSqlValue.hs
@@ -7,13 +7,11 @@ For license and copyright information, see the file COPYRIGHT
 -}
 
 module TestSqlValue where
-import TestInfrastructure
 import Test.QuickCheck hiding (Result)
 import Test.QuickCheck.Property (Result)
-import Test.QuickCheck.Tools
 import qualified Test.HUnit as HU
 import Database.HDBC
-import Data.Time.Format
+import Data.Time.Format (parseTime)
 import Data.Time.LocalTime
 import Database.HDBC.Locale (defaultTimeLocale, iso8601DateFormat)
 import Data.Maybe
@@ -22,12 +20,12 @@ instance Eq ZonedTime where
     a == b = zonedTimeToUTC a == zonedTimeToUTC b &&
              zonedTimeZone a == zonedTimeZone b
 
-toSql_Int :: Int -> Result
-toSql_Int x = toSql x @?= SqlInt64 (fromIntegral x)
+toSql_Int :: Int -> Property
+toSql_Int x = toSql x === SqlInt64 (fromIntegral x)
 
-fromSql_Int :: Int -> Result
+fromSql_Int :: Int -> Property
 fromSql_Int x = 
-    Right x @=? safeFromSql (SqlInt64 (fromIntegral x))
+    Right x === safeFromSql (SqlInt64 (fromIntegral x))
 
 testZonedTimeStr = "1989-08-01 15:33:01 -0500"
 testZonedTime :: ZonedTime
@@ -46,7 +44,10 @@ ztparsef =
 
 hut msg = HU.TestLabel msg . HU.TestCase
 
-allt = [q "toSql Int" toSql_Int,
-        q "safeFromSql Int" fromSql_Int,
-        hut "non-frac parse" ztparsenf,
-        hut "frac parse" ztparsef]
+allHUnitTests = [hut "non-frac parse" ztparsenf,
+                 hut "frac parse" ztparsef
+                ]
+
+allQuickCheckTests = [toSql_Int,
+                      fromSql_Int
+                     ]
